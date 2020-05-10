b42d7f287d7e9b7827f4fe7266e6a9bf38c428b0
Elena <helen_perets@mail.ru>
2020-05-10 13:47:11 +0300

    Initial commit

---
+++ src/main/java/geekbrains/lesson_1/FirstApp.java
@@ -0,0 +1,30 @@
+package geekbrains.lesson_1;
+
+
+public class FirstApp {
+
+    //1.Создать пустой проект в IntelliJ IDEA и прописать метод main();
+    public static void main(String[] args) {
+        //2.Создать переменные всех пройденных типов данных, и инициализировать их значения;
+        byte g = -120;
+        System.out.println(g);
+        int h = 120;
+        System.out.println(h);
+        short j = 10000;
+        System.out.println(j);
+        long k = -1000;
+        System.out.println(k);
+        float l = 12.65f;
+        System.out.println(l);
+        char z ='z';
+        System.out.println(z);
+        boolean is = true;
+        boolean isNot = false;
+        String str = "Привет";
+        System.out.println(str);
+
+    }
+
+    }
+
+
---
+++ src/main/java/part_2_lesson_1/ThirdTask.java
@@ -0,0 +1,16 @@
+package part_2_lesson_1;
+
+
+
+public class ThirdTask {
+    public static void main(String[] args){
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+        System.out.println(a * (b + (c / d)));
+    }
+
+}
+
+
